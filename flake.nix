866c459f566a13e6b266a0a98b728ec313a4f441
David Burke <david@burke.chat>
2026-01-09 15:48:04 +1100

    Removed ns for now

--- flake.nix
+++ flake.nix
@@ -66,7 +66,6 @@
                 ./modules/core/system-packages.nix
                 ./modules/core/unfree.nix
                 #./modules/core/xserver.nix
-                ./modules/programs/ns/ns.nix
                 home-manager.nixosModules.home-manager
                 {
                     home-manager = {
