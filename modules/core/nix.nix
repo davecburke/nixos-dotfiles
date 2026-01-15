c709ab5b7a5e6d0325dea6169c2137e0acfbfdbd
David Burke <david@burke.chat>
2026-01-15 16:18:08 +1100

    Created everforest theme nix

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,4 +1,5 @@
 { ... }:
 {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.download-buffer-size = 134217728; # 128 MB
 }
---
+++ modules/themes/stylix/stylix-everforest.nix
@@ -0,0 +1,41 @@
+{ inputs, pkgs, ... }:
+
+{
+    imports = [
+        inputs.stylix.nixosModules.stylix
+    ];
+
+    stylix = {
+        enable = true;
+
+        # Nord palette - using base16-schemes package
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
+
+        image = ../wallpaper/current.jpg;
+
+        fonts = {
+        serif = {
+            package = pkgs.noto-fonts;
+            name = "Noto Serif";
+        };
+        sansSerif = {
+            package = pkgs.noto-fonts;
+            name = "Noto Sans";
+        };
+        monospace = {
+            package = pkgs.nerd-fonts.jetbrains-mono;
+            name = "JetBrainsMono Nerd Font";
+        };
+        emoji = {
+            package = pkgs.noto-fonts-color-emoji;
+            name = "Noto Color Emoji";
+        };
+    };
+
+    cursor = {
+            package = pkgs.bibata-cursors;
+            name = "Bibata-Modern-Ice";
+            size = 24;
+        };
+    };
+}
