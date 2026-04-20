06a1b920320df00bb1bdb90be6ae879dbfbdfdbb
David Burke <david@burke.chat>
2026-04-20 16:00:50 +1000

    Added scanning apps

--- modules/core/printing.nix
+++ modules/core/printing.nix
@@ -1,4 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
     services.printing.enable = true;
+
+    # Scanning (network MFPs, eSCL/AirScan)
+    hardware.sane = {
+        enable = true;
+        extraBackends = [
+            pkgs.sane-airscan
+            pkgs.brscan5
+        ];
+    };
+
+    # Discovery for network devices (mDNS/DNS-SD), often needed for AirScan.
+    services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+        simple-scan
+    ];
 }
